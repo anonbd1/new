9905d532c47e9db1a052be13a7cbde32d6ae6d00
AnonBD <99282155+anonbd1@users.noreply.github.com>
2022-03-28 03:30:33 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@anonbd</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon.png">
+    <link rel="stylesheet" href="assets/css/pace.css">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>⧼@̼⧽⧼A̼⧽⧼N̼⧽⧼O̼⧽⧼N̼⧽⧼B̼⧽⧼D̼⧽</title>
+    <!-- CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600%7CRoboto:400" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <!-- Head Libs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    <script data-pace-options='{ "ajax": false, "selectors": [ "img" ]}' src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
+    <script>
+var myVar=setInterval(function(){myTimer()},1000);
+function myTimer() {
+    var d = new Date();
+    document.getElementById("horas").innerHTML = d.toLocaleTimeString();
+}
+</script>
+<script type="text/javascript">
+function Mudaestado(el) {
+        var display = document.getElementById(el).style.display;
+        if(display == "none")
+            document.getElementById(el).style.display = 'block';
+        else
+            document.getElementById(el).style.display = 'none';
+    }
+</script>
+</head>
 
-    <!-- Bootstrap -->
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
- <body>
+<body>
         <!-- /.site-sidebar -->
         <main class="main-wrapper clearfix">
             <!-- Page Title Area -->
             <div class="row page-title clearfix">
                 <div class="page-title-left">
                     <center><h4 class="page-title-heading mr-0 mr-r-10">⧼@̼⧽⧼A̼⧽⧼N̼⧽⧼O̼⧽⧼N̼⧽⧼B̼⧽⧼D̼⧽</h4></center>
-                    <p class="page-title-description mr-0 d-none d-md-inline-block"></p>
                 </div>
             </div>
-            <div class="widget-list row">
-                
-                <!-- /.widget-holder -->
-                <div class="widget-holder widget-full-height widget-flex col-lg-6">
-                    <div class="widget-bg">
-                        <div class="widget-heading">
-                            <div class="widget-graph-info">
-                                <div class="dropdown"><a href="javascript:void(0)" class="dropdown-toggle text-muted fs-16" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
-                                    <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#">Action</a>  <a class="dropdown-item" href="#">Another action</a>  <a class="dropdown-item" href="#">Something else here</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.widget-graph-info -->
-                        </div>
                         <!-- /.widget-heading -->
                         <div class="widget-body">
-                            <button type="button" id="sh_apr" onclick="Mudaestado('aprovadasapp');" class="btn btn-success">APPROVED [ <span id="aprovada_conta">0</span> ]</button>
+                            <button type="button" id="sh_apr" onclick="Mudaestado('aprovadasapp');" class="btn btn-blue">APPROVED [ <span id="aprovada_conta">0</span> ]</button>
 
                             <br>
 
@@ -60,18 +63,6 @@
                     </div>
                     <!-- /.widget-bg -->
                 </div>
-                <!-- /.widget-holder -->
-                <div class="widget-holder widget-full-content widget-full-height col-lg-6">
-                    <div class="widget-bg">
-                        <div class="widget-heading">
-                            <div class="widget-graph-info">
-                                <div class="dropdown"><a href="javascript:void(0)" class="dropdown-toggle text-muted fs-16" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
-                                    <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#">Action</a>  <a class="dropdown-item" href="#">Another action</a>  <a class="dropdown-item" href="#">Something else here</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.widget-graph-info -->
-                        </div>
                         <!-- .widget-heading -->
                         <div class="widget-body">
                             <div class="container-fluid pd-20">
@@ -79,7 +70,7 @@
                                     <div class="col">
                                         
 
-                                    <textarea id="lista" placeholder="0000000000000000|00|00|0000" class="form-control" style="resize:none;outline:none;width:100%; height:150px;"></textarea><br>
+                                    <textarea id="lista" placeholder="" class="form-control" style="resize:none;outline:none;width:100%; height:150px;"></textarea><br>
 
                                     <center><button type="button" id="iniciar" onclick="start()" class="btn btn-blue">Start</button> ㅤㅤㅤ ㅤ ㅤ ㅤㅤㅤ ㅤ ㅤ <button type="button" onclick="stop()" id="parar" class="btn btn-danger">Stop</button></center>
 
@@ -96,7 +87,6 @@
                         <!-- /.widget-body -->
                     </div>
                     <!-- /.widget-bg -->
-                </div>
     <center>STATUS: <span class="bagde badge-pill badge-primary" id="demo">WAITING</span> - 
 APPROVED: <span id="CLIVE" class="badge badge-blue">0</span> - 
 REJECTED: <span id="CDIE" class="badge badge-danger">0</span> - 
@@ -105,13 +95,11 @@ TOTAL: <span id="carregada" class="badge badge-white">0</span></center>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <br>
 <center><span class="badge badge-blue">Checker Modified by:</span>  <span class="badge badge-success"><a href = "https://telegram.dog/hackerbd1" style="color:white">⧼@̼⧽⧼A̼⧽⧼N̼⧽⧼O̼⧽⧼N̼⧽⧼B̼⧽⧼D̼⧽</a></span></center>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+    <script src="assets/js/custom.js"></script>
     <script src="js.js"></script>
     <script src="jquery-ui.js"></script>
     <script src="jquery.js"></script>
-  </body>
+</body>
+
 </html>
